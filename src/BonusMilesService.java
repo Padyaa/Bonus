12365bd9357529c520f9387e359a20ad6c478b5b
Padyaa <zehopa@mail.ru>
2022-06-08 20:02:26 +0300

    first commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,14 @@
+public class BonusMilesService {
+    public int calculate(int price) {
+        int cost = 20;
+        int bonusMiles = price / cost;
+        return bonusMiles;
+    }
+}
+
+
+
+
+
+
+
